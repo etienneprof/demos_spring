b91e234256cef3a017c2b9b132fefad00f0a1b1c
Etienne CASSIN <ecassin@campus-eni.fr>
2025-07-08 09:56:50 +0200

    first unit tests

--- src/test/java/fr/eni/demospringboot/dal/EmployeRepositoryTest.java
+++ src/test/java/fr/eni/demospringboot/dal/EmployeRepositoryTest.java
@@ -1,7 +1,10 @@
 package fr.eni.demospringboot.dal;
 
 import fr.eni.demospringboot.bo.Employe;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,12 +19,14 @@ import static org.junit.jupiter.api.Assertions.*;
  * @AutoConfigureTestDatabase : pour supprimer le comportement par défaut qui va chercher H2 (une BDD embarquée)
  */
 @DataJpaTest @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class EmployeRepositoryTest {
 
     @Autowired
     private EmployeRepository repository;
 
     @Test
+    @Order(3)
     public void test_save() {
         Employe employe = Employe.builder()
                 .nom("CASSIN")
@@ -31,6 +36,27 @@ class EmployeRepositoryTest {
         repository.save(employe);
         assertNotNull(employe.getId());
         System.out.println(employe.getId());
+    }
+
+    @Test
+    @Order(1)
+    public void test_findById() {
+        jeuDeDonnees();
+        Employe emp = repository.findById(1).orElseThrow();
+        assertEquals("BAILLE",emp.getNom());
+        assertEquals("Anne-Lise",emp.getPrenom());
+        assertEquals("abaille@campus-eni.fr",emp.getEmail());
+        assertEquals("ENI_ECOLE_12398",emp.getImmatriculation());
+        assertEquals("02XXXXXXXX",emp.getNumDom());
+    }
+
+    @Test
+    @Order(2)
+    public void test_findAll() {
+        jeuDeDonnees();
+        List<Employe> employees = repository.findAll();
+        assertNotNull(employees);
+        assertEquals(3,employees.size());
     }
 
     private void jeuDeDonnees() {
